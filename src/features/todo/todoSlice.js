06352ab52f2d4c530a6d44133c2f6e09dc57c400
daniyal <mohddaniyal365@gmail.com>
2023-10-12 05:54:23 +0530

    Initial commit

---
+++ src/features/todo/todoSlice.js
@@ -0,0 +1,30 @@
+import {createSlice,nanoid} from '@redux/toolkit'
+
+const initialState={
+    todos:[{id:1,text:"daniyal"}]
+}
+
+export const todoSlice=createSlice({
+    name:'todo',
+    initialState,
+    reducers:{
+        AddTodo:(state,action)=>{
+            const todo={
+                id:nanoid(),
+                text:action.payload
+                
+            }
+            state.todos.push(todo)
+
+        },
+        RemoveTodo:(state,action)=>{
+            state.todos=state.todos.filter((todo)=>todo.id !== action.payload)
+
+        }
+        
+    }
+    
+})
+
+export const {RemoveTodo,AddTodo}=todoSlice.actions
+export default todoSlice.reducer
